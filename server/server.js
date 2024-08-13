98f6a2b6d3adabceed4b78697d311ffbf956c85b
Jay <jayden_benston@yahoo.com>
2024-08-13 11:07:06 -0400

    setup client page

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const db = require('./config/connection');
 const api_routes = require('./routes/api_routes');
@@ -6,9 +7,15 @@ const api_routes = require('./routes/api_routes');
 const app = express();
 const PORT = 3001;
 
+app.use(express.static('../client/'));
+
 app.use(express.json());
 
 app.use('/api', api_routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 db.once('open', () => {
   console.log('DB locked in');
