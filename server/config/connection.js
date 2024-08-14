96884039ed650293d78c4630014ee35052835101
Jay <jayden_benston@yahoo.com>
2024-08-14 14:08:43 -0400

    debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+console.log(process.env.DB_URL);
 
-mongoose.connect('mongodb://localhost:27017/transformers_db');
+mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/transformers_db');
 
 module.exports = mongoose.connection;
